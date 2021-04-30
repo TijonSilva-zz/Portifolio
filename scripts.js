26ea9dbecf84b1a3614fa0d37a6e964849ac9521
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-04-30 00:03:30 -0300

    nao to conseguindo fazer o click com js funcionar em todos os cards

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
-const modalOverlay = document.querySelector('modal-overlay')
+const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
+const cards1 = document.querySelectorAll('.card1')
+const cards2 = document.querySelectorAll('.card2')
+const cards3 = document.querySelectorAll('.card3')
+const cards4 = document.querySelectorAll('.card4')
+const cards5 = document.querySelectorAll('.card5')
 
-for (let card of cards) [
+for (const card of cards) [
  card.addEventListener("click", function (){
   modalOverlay.classList.add('active')
  })
