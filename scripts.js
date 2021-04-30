a9c7c5fa3902c0d3e072dfbcf7e8ea31e85e4921
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-04-29 21:44:53 -0300

    removendo tudo.

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const modalOverlay = document.querySelector('.modal-ovarlay')
-const cards = document.querySelectorAll('.card')
-
-for (let card of cards) {
-card.addEventListener("click", function(){
- modalOverlay.classList.add('active')
-
- })
-}
