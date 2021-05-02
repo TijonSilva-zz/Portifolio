f00922cb9ee4694a47d7ff475fd9d6dd6df8f4cc
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-05-02 19:58:43 -0300

    js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,9 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
-const cards1 = document.querySelectorAll('.card1')
-const cards2 = document.querySelectorAll('.card2')
-const cards3 = document.querySelectorAll('.card3')
-const cards4 = document.querySelectorAll('.card4')
-const cards5 = document.querySelectorAll('.card5')
+ const modalOverlay = document.querySelector('.modal-overlay')
+ const cards = document.querySelectorAll ('.card1')
 
-for (const card of cards) [
- card.addEventListener("click", function (){
-  modalOverlay.classList.add('active')
- })
-]
+ for (const card of cards) {
+  card.addEventListener("click", function (){
+   modalOverlay.classList.add('active')
+
+  })
+ }
